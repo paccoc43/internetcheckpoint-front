22635d8efe762d703d3606974b6d2b98c01b7c4b
paccoc43 <pacotecnolofuerte@gmail.com>
2024-02-22 23:00:05 +0100

    Creando nuevos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,23 @@
 import { Routes } from '@angular/router';
-import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {ListaUsuariosComponent} from './components/lista-usuarios/lista-usuarios.component';
 
 export const routes: Routes = [
-    { path: 'lista-usuarios', component: ListaUsuariosComponent },
+    { 
+        path: 'home',
+        component: HomePageComponent ,
+        children: [
+            {
+                path: 'lista-usuarios',
+                title: 'lista-usuarios',
+                component: ListaUsuariosComponent
+                    
+            },
+        ] 
+    },
+    { 
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
 ];
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [
+    RouterModule,
+    NavbarComponent
+  ],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+
+}
