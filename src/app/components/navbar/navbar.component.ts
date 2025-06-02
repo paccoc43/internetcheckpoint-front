2f76607554d74c5771ae5b03cc1338a7375fbc42
paccoc43 <pacotecnolofuerte@gmail.com>
2025-06-02 11:46:26 +0200

    Habilita el boton logout el cual elimina los datos de sesion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
+    CommonModule,
     RouterModule
   ],
   templateUrl: './navbar.component.html',
@@ -12,5 +15,16 @@ import { RouterModule } from '@angular/router';
 })
 export class NavbarComponent {
 
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
+
+    logout() {
+    localStorage.removeItem('usuario');
+    // Si guardas un token, bórralo también:
+    // localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
 
